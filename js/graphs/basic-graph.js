f68f4abe11eaf7c0d24950b0ca93232d39308e22
Your Name <you@example.com>
2024-10-30 14:30:16 +0300

    graphs js

---
+++ js/graphs/basic-graph.js
@@ -0,0 +1,21 @@
+var arrayVertex = ['A', 'B', 'C', 'D'];
+var arrayConnection = [
+    [0, 1, 1, 1],
+    [1, 0, 1, 0],
+    [1, 1, 0, 0],
+    [1, 0, 0, 0]
+];
+
+const printConnections = () => {
+    for (let i = 0; i < arrayVertex.length; i++) {
+        let strCon = `${arrayVertex[i]}: `;
+        for (let row = 0; row < arrayConnection.length; row++) {
+            if (arrayConnection[row][i] == 1) {
+                strCon += `${arrayVertex[row]}, `;
+            }
+        }
+        console.log(strCon)
+    }
+}
+
+printConnections();
